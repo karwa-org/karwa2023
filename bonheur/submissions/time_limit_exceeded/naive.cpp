e8fbf5a516b971cf39bd497925391045879e7efe
allEyezOnCode <alexandredewilde2001@gmail.com>
2023-04-14 19:35:26 +0200

    update problems

---
+++ bonheur/submissions/time_limit_exceeded/naive.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    int n, k; cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] + a[j] == k) {
+                cout << "yes" << endl;
+                exit(0);
+            }
+        }
+    }
+    cout << "no" << endl;
+
+}
