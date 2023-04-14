c24b29785a692435e4390c99f79c70858f27b7af
ChristopheG <61288881+chrisgdt@users.noreply.github.com>
2023-04-14 17:15:07 +0200

    eulerian cycle problem (new name : circuit confort)

---
+++ circuitconfort/submissions/accepted/christophe_sol.py
@@ -0,0 +1,16 @@
+def dfs(graph, u, visited):
+    visited[u] = True
+    for v in graph[u]:
+        if not visited[v]:
+            dfs(graph, v, visited)
+n, start, graph = int(input()), input(), __import__("collections").defaultdict(list)
+for _ in range(n):
+    a, b = input().split()
+    graph[a].append(b)
+    graph[b].append(a)
+visited = {v: False for v in graph}
+dfs(graph, start, visited)
+if any(not visited[v] for v in graph):
+    print("impossible")
+else:
+    print("impossible" if any(len(graph[v]) % 2 != 0 for v in graph) else "ok")
