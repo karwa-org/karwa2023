c08b2c3b1ee247077810d73adb267f1e41fcc991
ChristopheG <61288881+chrisgdt@users.noreply.github.com>
2023-04-15 19:00:53 +0200

    fix code mirror

--- miroirmagique/submissions/accepted/christophe_sol.py
+++ miroirmagique/submissions/accepted/christophe_sol.py
@@ -1,1 +1,1 @@
-print(2*int(input())*__import__("math").tan(__import__("math").radians(int(input()))))
+print(round(2*int(input())*__import__("math").tan(__import__("math").radians(int(input()))),6)
