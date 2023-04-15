943efe3c7350fac7ef40ecd4eb5194bdaf36cf7d
Shinkumons <210069@umons.ac.be>
2023-04-15 14:00:18 +0200

    Koalation problem

---
+++ koalation/submissions/accepted/koalation.py
@@ -0,0 +1,28 @@
+nbrs = [2, 4, 5, 8]
+
+primes = [2, 3]
+
+def isPrime(n):
+    for i in primes:
+        if i*i > n:
+            break
+        if i%n == 0:
+            return False
+    return True
+
+for j in range(10000):
+    if isPrime(5 + j*6):
+        primes.append(5 + j*6)
+    if isPrime(7 + j*6):
+        primes.append(7 + j*6)
+
+
+for i, n in enumerate(nbrs):
+    if n == 1: nbrs[i] = (1,1)
+    else:
+        for p in primes:
+            if n % p == 0:
+                nbrs[i] = (p, n)
+                break
+nbrs.sort()
+print(' '.join(map(lambda x: str(x[1]), nbrs)))
