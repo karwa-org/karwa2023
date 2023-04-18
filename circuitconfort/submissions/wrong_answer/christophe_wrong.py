719e0b1d9b3600dc57eaf90f9a9a90d858e77813
chrisgdt <chgrandmont@gmail.com>
2023-04-18 11:26:57 +0200

    Attempt to make workflow run !

--- circuitconfort/submissions/wrong_answer/christophe_wrong.py
+++ circuitconfort/submissions/wrong_answer/christophe_wrong.py
@@ -1,14 +1,16 @@
-from collections import defaultdict
 def dfs(graph, u, visited):
     visited[u] = True
     for v in graph[u]:
         if not visited[v]:
             dfs(graph, v, visited)
-n, start, graph = int(input()), input(), defaultdict(list)
+n, start, graph = int(input()), input(), {}
+graph[start] = []
 for _ in range(n):
     a, b = input().split()
+    if b not in graph: graph[b] = []
+    if a not in graph: graph[a] = []
     graph[a].append(b)
-visited = defaultdict(lambda: False)
+visited = {v: False for v in graph}
 dfs(graph, start, visited)
 if any(not visited[v] for v in graph):
     print("impossible")
